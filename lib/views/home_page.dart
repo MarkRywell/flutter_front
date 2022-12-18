cc8af9de166e110610a36f17142435eea4938cbc
MakuRui <marklouienambatac@gmail.com>
2022-12-18 17:24:03 +0800

    Added item class

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -13,7 +13,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: Text('Testing Appbar'),
       ),
     );
   }
