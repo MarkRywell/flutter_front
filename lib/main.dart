01607536ab9100466389397d9c16095b511a4c94
MarkRywell <gopro855@gmail.com>
2022-12-21 18:43:27 +0800

    Added Animated Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_front/views/home_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:lottie/lottie.dart';
 
 void main() async {
 
@@ -10,6 +12,11 @@ void main() async {
   runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomePage()
+        home: AnimatedSplashScreen(
+          splash: Image.asset('assets/OnlySells.png'),
+          splashIconSize: 1000,
+          duration: 4000,
+          nextScreen: const HomePage(),
+        )
       ));
 }
