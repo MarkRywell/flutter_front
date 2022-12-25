e9f0309224c3256064c0e6f5152b1413ef89b4ce
John Ray Canete <johnraycaete@gmail.com>
2022-12-25 19:09:00 +0800

    Added some changes in Register Page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_front/views/login_page.dart';
+import 'package:flutter_front/views/register_page.dart';
 
 void main() async {
 
@@ -14,11 +15,11 @@ void main() async {
           primarySwatch: Colors.blue
         ),
         debugShowCheckedModeBanner: false,
-        home: AnimatedSplashScreen(
+        home: RegisterPage()/*AnimatedSplashScreen(
           splash: Image.asset('assets/OnlySells.png'),
           splashIconSize: 1000,
           duration: 4000,
           nextScreen: const LoginPage(),
-        )
+        )*/
       ));
 }
